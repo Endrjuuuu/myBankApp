f5b882015801bd869b14f739a4418b6c96bed4cd
AndrzejW <witkowskiandrzej8@gmail.com>
2025-10-25 20:09:39 +0200

    feat: refactor bank account component to use reactive forms for withdrawal functionality

--- src/app/pages/dashboard/components/bank-account/bank-account.component.ts
+++ src/app/pages/dashboard/components/bank-account/bank-account.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BalancePipe } from '../../pipes/balance.pipe';
 import { BankAccount } from '../../models/dashboard.models';
 
 @Component({
   selector: 'app-bank-account',
   standalone: true,
-  imports: [CommonModule, FormsModule, BalancePipe],
+  imports: [CommonModule, ReactiveFormsModule, BalancePipe],
   templateUrl: './bank-account.component.html',
   styleUrl: './bank-account.component.scss',
 })
@@ -15,9 +15,20 @@ export class BankAccountComponent {
   @Input() account!: BankAccount;
   @Output() withdrawMoney$ = new EventEmitter<number>();
 
-  moneyToWithdraw = 0;
+  form = new FormGroup({
+    withdraw: new FormControl(0, {validators: [Validators.required, Validators.min(1), Validators.max(this.account.balance)]})}
+  )
 
+  get withdrawControl(): FormControl{
+    return this.form.get('withdraw') as FormControl
+  }
+
+  get withdrawControlValue() : number{
+    return this.withdrawControl.value;
+  }
+ 
   withdrawMoney() {
-    this.withdrawMoney$.next(this.moneyToWithdraw);
+    this.withdrawMoney$.next(this.withdrawControlValue);
+    this.withdrawControl.setValue(null);
   }
 }
